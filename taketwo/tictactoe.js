edb3e987b4b51de28c818123b47a00e0ab4af2fb
Chris Gastaldo <chris.gastaldo@gmail.com>
2024-09-05 06:46:01 -0400

    Changed name of marker labels. Added inputs within labels for markers and text fields. Created function to get radio values and player names

--- taketwo/tictactoe.js
+++ taketwo/tictactoe.js
@@ -1,4 +1,3 @@
-
 const gameBoard = document.querySelector('#gameBoard');
 
 let turn = 'X';
@@ -11,8 +10,6 @@ function createPlayer(name, marker){
     this.marker = marker;
 }
 
-
-// and get data from users
 function createInfoDisplay(){
     const infoPanel = document.querySelector('#information');
     const infoPanelPlayers = document.createElement('div');
@@ -36,36 +33,37 @@ function buildPlayerInfo(infoPanelPlayers){
     player1NameLabel.textContent = "Enter Player 1 Name";
     player1NameInput.setAttribute('type', 'text');
     player1NameInput.setAttribute('id', 'player1Name');
+    player1NameInput.setAttribute('name', 'textFieldName');
     player2NameLabel.textContent = "Enter Player 2 Name";
     player2NameInput.setAttribute('type', 'text');
     player2NameInput.setAttribute('id', 'player2Name');
+    player2NameInput.setAttribute('name', 'textFieldName');
     infoPanelPlayers.append(player1NameLabel);
-    infoPanelPlayers.append(player1NameInput);
+    player1NameLabel.append(player1NameInput);
     infoPanelPlayers.append(player2NameLabel);
-    infoPanelPlayers.append(player2NameInput);
+    player2NameLabel.append(player2NameInput);
 
     const markerDescription = document.createElement('p');
     markerDescription.textContent = "Player 1, please select a marker."
     + " Player 2 will automatically be assigned the other marker.";
     infoPanelPlayers.append(markerDescription);
 
-    const markerNameX = document.createElement('Label');
-    const markerNameO = document.createElement('Label');
+    const markerXLabel = document.createElement('Label');
+    const markerOLabel = document.createElement('Label');
     const markerSelectX = document.createElement('input');
     const markerSelectO = document.createElement('input');
-    markerNameX.textContent = "X";
+    markerXLabel.textContent = "X";
     markerSelectX.setAttribute('type', 'radio');
-    markerSelectX.setAttribute('id', 'markerTypeX');
-    markerNameO.textContent = "O";
+    markerSelectX.setAttribute('id', 'X');
+    markerSelectX.setAttribute('name', 'markerType');
+    markerOLabel.textContent = "O";
     markerSelectO.setAttribute('type', 'radio');
-    markerSelectO.setAttribute('id', 'markerTypeO');
-    infoPanelPlayers.append(markerSelectX);
-    infoPanelPlayers.append(markerNameX);
-    infoPanelPlayers.append(markerSelectO);
-    infoPanelPlayers.append(markerNameO);
-
-
-
+    markerSelectO.setAttribute('id', 'O');
+    markerSelectO.setAttribute('name', 'markerType');
+    infoPanelPlayers.append(markerXLabel);
+    markerXLabel.append(markerSelectX);
+    infoPanelPlayers.append(markerOLabel);
+    markerOLabel.append(markerSelectO);
 }
 
 function buildGameInfo(infoPanelGame) {
@@ -80,24 +78,49 @@ function buildGameInfo(infoPanelGame) {
     infoPanelGame.append(scoreDisplay);
     infoPanelGame.append(currentTurn);
     infoPanelGame.append(startGameBtn);
-
-
 }
 
-
 function assignPlayerInfo(){
-
     const player1Name = document.querySelector('player1Name');
     const player2Name = document.querySelector('player2Name');
     const markerSelectX = document.querySelector('markerTypeX');
     const markerSelectO = document.querySelector('markerTypeO');
+}
+
+function getRadioValue(){
+    const markers = document.getElementsByName('markerType');
+    for (i = 0; i<markers.length; i++){
+        if (markers[i].checked){
+            return markers[i].id;
+        }
+    }
+}
+
+function getPlayerName(playerNumber){
+    const textFieldName = document.getElementsByName('textFieldName');
+    if (playerNumber === 'player1'){
+        return textFieldName[0].value;
+    }
+    else if (playerNumber === 'player2'){
+        return textFieldName[1].value;
+    }
 
 }
+
+//create user1 and user 2 based on below
+//announce winner
+//update scoreboard
+//disable start button after clicking 
+//enable start button after finishing game
+//need to overwrite within field and not create one below
 
 const startGameBtn = document.querySelector('button');
 startGameBtn.disabled = true;
 startGameBtn.disabled = false;
 startGameBtn.addEventListener('click', () => {
+    getRadioValue();
+    getPlayerName("player1");
+    getPlayerName("player2");
     createBoard()
 })
 
@@ -118,8 +141,6 @@ function createBoard(){
         gameBoard.append(cellContent);
     })
 }
-
-
 
 function addTurn(e){
     currentTurn = document.querySelector('.turn')
@@ -143,9 +164,7 @@ function checkScore() {
     const winCombinations = [
         [0, 1, 2], [3, 4, 5],[6, 7, 8],
         [0, 3, 6], [1, 4, 7],[2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
-    
+        [0, 4, 8], [2, 4, 6]];
     for (let i = 0; i<winCombinations.length; i++){
         winCombination = winCombinations[i];
         if(winCombination.every(cellLocation => 
